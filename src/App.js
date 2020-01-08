cc7328f5b9656140d5f996a1e7c11114eea9193d
Flavia Costa <flavia.costa@talkdesk.com>
2020-01-08 14:41:50 +0000

    Add html to AppList component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./styles.css";
 import CategoriesMenu from "./components/CategoriesMenu";
 import SearchApp from "./components/SearchApp";
 import Pagination from "./components/Pagination";
+import AppList from "./components/AppList";
 
 const App = () => {
   return (
@@ -14,107 +15,7 @@ const App = () => {
           </nav>
           <section class="apps-list">
             <SearchApp />
-            <ul>
-              <li>
-                <div class="app-item">
-                  <div class="box-info">
-                    <div class="box-info--content">
-                      <div class="description">
-                        <h1>Voice Report</h1>
-                        <p>Calls reporting and analytics of your calls.</p>
-                      </div>
-                      <div class="tags">
-                        <span>Voice Analytics</span> / <span>Reporting</span> /{" "}
-                        <span>Optimization</span>
-                      </div>
-                    </div>
-                    <div className="box-info--footer">
-                      <ul>
-                        <li>
-                          <span>Trial</span>{" "}
-                          <h3>
-                            Free<sup></sup>
-                          </h3>
-                        </li>
-                        <li>
-                          <span>Professional</span>{" "}
-                          <h3>
-                            35.00<sup>€</sup>
-                          </h3>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="app-item">
-                  <div class="box-info">
-                    <div class="box-info--content">
-                      <div class="description">
-                        <h1>Power Dialer</h1>
-                        <p>
-                          Auto dialer that will help increase your connect rates
-                          and talk time.
-                        </p>
-                      </div>
-                      <div class="tags">
-                        <span>Dialer</span>
-                      </div>
-                    </div>
-                    <div class="box-info--footer">
-                      <ul>
-                        <li>
-                          <span>Trial</span>{" "}
-                          <h3>
-                            Free<sup></sup>
-                          </h3>
-                        </li>
-                        <li>
-                          <span>Professional</span>{" "}
-                          <h3>
-                            45.00<sup>€</sup>
-                          </h3>
-                        </li>
-                        <li>
-                          <span>Premium</span>{" "}
-                          <h3>
-                            60.00<sup>€</sup>
-                          </h3>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="app-item">
-                  <div class="box-info">
-                    <div class="box-info--content">
-                      <div class="description">
-                        <h1>Smart Text</h1>
-                        <p>
-                          Use SMS to help you communicate with your customers.
-                        </p>
-                      </div>
-                      <div class="tags">
-                        <span>Channels</span>
-                      </div>
-                    </div>
-                    <div class="box-info--footer">
-                      <ul>
-                        <li>
-                          <span>Trial</span>{" "}
-                          <h3>
-                            Free<sup></sup>
-                          </h3>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </li>
-            </ul>
+            <AppList />
             <Pagination />
           </section>
         </div>
